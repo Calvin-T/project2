2860c8e6e6a21da70bffff4a771a7de4678dd777
csyang98 <43451330+csyang98@users.noreply.github.com>
2019-03-17 20:03:17 -0400

    added ts_edu and ts_com

--- rs.py
+++ rs.py
@@ -2,8 +2,8 @@ import socket
 import sys
 
 RS_DNS_Table = {}
-com_hostname = ""
-edu_hostname = ""
+com_IP_addr = ""
+edu_IP_addr = ""
 
 
 lines = [line.rstrip('\r\n') for line in open("PROJ2-DNSRS.txt")]
@@ -11,15 +11,15 @@ for line in lines:
 	lineSplit = line.split()
 	if lineSplit[2] == "NS":
 		domainSplit = lineSplit[0].split(".")
-		if lineSplit[0].endswith("edu"):
-			edu_hostname = lineSplit[0]
-		if lineSplit[0].endswith("com"):
-			com_hostname = lineSplit[0]
+		if lineSplit[0].endswith(".edu"):
+			edu_IP_addr = lineSplit[1]
+		if lineSplit[0].endswith(".com"):
+			com_IP_addr = lineSplit[1]
 	else:
-		RS_DNS_Table[lineSplit[0]] = [lineSplit[1], lineSplit[2]]
+		RS_DNS_Table[lineSplit[0].lower()] = line
 
-print("com host: " + com_hostname)
-print("edu host: " + edu_hostname)
+print(".com host: " + com_IP_addr)
+print(".edu host: " + edu_IP_addr)
 
 if len(sys.argv) == 4:
 	try:
@@ -47,76 +47,57 @@ print("[RS]: Got a connection request from a client at {}".format(addr))
 
 #msg = "Connected to RS Server"
 #csockid.send(msg.encode('utf-8'))
+try:
+	tsEduSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as err:
+	print('socket open error: {} \n'.format(err))
+	exit()
+
+# tsEduServer_addr = socket.gethostbyname("localhost") #switch to edu_hostname
+tsEduServer_binding = (edu_IP_addr, tsEduListenPort)
+tsEduSocket.connect(tsEduServer_binding)
+
+try:
+	tsComSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as err:
+	print('socket open error: {} \n'.format(err))
+	exit()
+
+# tsComServer_addr = socket.gethostbyname("localhost") #switch to com_hostname
+tsComServer_binding = (com_IP_addr, tsComListenPort)
+tsComSocket.connect(tsComServer_binding)
 
 while True:
 	data_from_client = csockid.recv(200)
 	recv_msg = data_from_client.decode('utf-8')
-	
-	recv_msg = recv_msg.lower()
 
 	print("[C]: "+ recv_msg)
 
 	if recv_msg == "done":
-		try:
-			tsEduSocket.send("done")
-			tsComSocket.send("done")
-		except:
-			# no need to open sockets since they wont be open
-			pass
-
+		tsEduSocket.send(recv_msg.encode('utf-8'))
+		tsComSocket.send(recv_msg.encode('utf-8'))
 		break
 	else:
-		if recv_msg in RS_DNS_Table:
-			values= RS_DNS_Table[recv_msg]
-			send_msg = recv_msg + " " + values[0]+ " " + values[1]
-			print("[RS]: "+ send_msg)
+		if recv_msg.lower() in RS_DNS_Table:
+			send_msg = RS_DNS_Table[recv_msg.lower()]
+			print("[RS] to [C]: "+ send_msg)
 			csockid.send(send_msg.encode('utf-8'))
 		else:
 			if recv_msg.endswith("edu"):
-				#contact ts_edu
-				for attempt in range(2):
-					try:
-						print("[RS]: " + recv_msg)
-						tsEduSocket.send(recv_msg)
-						data_from_tsedu = tsEduSocket.recv(200)
-						msg_received = data_from_tsedu.decode('utf-8')
-						print("[TSedu]: " + msg_received)
-						csockid.send(data_from_tsedu)
-						break;
-					except:
-						#open socket
-						try:
-							tsEduSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-						except socket.error as err:
-							print('socket open error: {} \n'.format(err))
-							exit()
-
-						tsEduServer_addr = socket.gethostbyname("localhost") #switch to edu_hostname
-						tsEduServer_binding = (tsEduServer_addr, tsEduListenPort)
-						tsEduSocket.connect(tsEduServer_binding)
+				print("[RS] to [edu]:  " + recv_msg)
+				tsEduSocket.send(recv_msg.encode('utf-8'))
+				data_from_tsedu = tsEduSocket.recv(200)
+				msg_received = data_from_tsedu.decode('utf-8')
+				print("[edu]: " + msg_received)
+				csockid.send(msg_received.encode('utf-8'))
 
 			elif recv_msg.endswith("com"):
-				#contact ts_com
-				for attempt in range(2):
-					try:
-						print("[RS]: " + recv_msg)
-						tsComSocket.send(recv_msg)
-						data_from_tscom = tsComSocket.recv(200)
-						msg_received = data_from_tscom.decode('utf-8')
-						print("[TScom]: " + msg_received)
-						csockid.send(data_from_tscom)
-						break;
-					except:
-						#open socket
-						try:
-							tsComSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-						except socket.error as err:
-							print('socket open error: {} \n'.format(err))
-							exit()
-
-						tsComServer_addr = socket.gethostbyname("localhost") #switch to com_hostname
-						tsComServer_binding = (tsComServer_addr, tsComListenPort)
-						tsComSocket.connect(tsComServer_binding)
+				print("[RS] to [com]: " + recv_msg)
+				tsComSocket.send(recv_msg.encode('utf-8'))
+				data_from_tscom = tsComSocket.recv(200)
+				msg_received = data_from_tscom.decode('utf-8')
+				print("[com]: " + msg_received)
+				csockid.send(msg_received.encode('utf-8'))
 			else:
 				send_msg = recv_msg + " - Error:HOST NOT FOUND"
 				print("[RS]: " + send_msg)
--- ts_edu.py
+++ ts_edu.py
@@ -6,11 +6,11 @@ TSedu_DNS_Table = {}
 lines = [line.rstrip('\r\n') for line in open("PROJ2-DNSTSedu.txt")]
 for line in lines:
 	lineSplit = line.split()
-	TSedu_DNS_Table[lineSplit[0]] = [lineSplit[1], lineSplit[2]]
+	TSedu_DNS_Table[lineSplit[0].lower()] = line
 
 if len(sys.argv) == 2:
 	try:
-		tsEduListenPort = int(sys.argv[1])
+		tsComListenPort = int(sys.argv[1])
 	except ValueError:
 		exit(1)
 else:
@@ -18,31 +18,42 @@ else:
 	exit(1)
 
 try:
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("[S]: Server socket created")
+	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	print("[S]: Server socket created")
 except socket.error as err:
-    print('socket open error: {}\n'.format(err))
-    exit()
+	print('socket open error: {}\n'.format(err))
+	exit()
 
-server_binding = ('', tsEduListenPort)
+#set up the server socket
+server_binding = ('', tsComListenPort)
 serverSocket.bind(server_binding)
 serverSocket.listen(1)
-
+host = socket.gethostname()
+print("[S]: Server host name is {}".format(host))
+localhost_ip = (socket.gethostbyname(host))
+print("[S]: Server IP address is {}".format(localhost_ip))
 csockid, addr = serverSocket.accept()
+print("[S]: Got a connection request from a client at {}".format(addr))
 
 while True:
-	data_from_rs = csockid.recv(200)
-	recv_msg = data_from_rs.decode('utf-8')
-	print("[RS]: " + recv_msg)
+	data_from_client = csockid.recv(200)
+	recv_msg = data_from_client.decode('utf-8')
+	print("[C]: "+ recv_msg)
 
 	if recv_msg == "done":
 		break
 	else:
-		send_msg = "Message from ts_edu"
-		print("[TSedu]: " + send_msg)
-		csockid.send(send_msg.encode('utf-8'))
-	print("")
+		if recv_msg.lower() in TSedu_DNS_Table:
+			send_msg =TSedu_DNS_Table[recv_msg.lower()]
+			print("[S]: "+ send_msg)
+			csockid.send(send_msg.encode('utf-8'))
+		else:
+			send_msg = recv_msg + " - Error:HOST NOT FOUND"
+			print("[S]: " + send_msg)
+			csockid.send(send_msg.encode('utf-8'))
 
 
+
+# Close the server socket
 serverSocket.close()
 exit()
